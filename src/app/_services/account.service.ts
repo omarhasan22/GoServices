58bb468b62b77c9ca1f81a8dbe45c0fe94c3e74e
your <omar.hasan3894@gmail.com>
2023-12-22 00:34:13 +0200

    221223

---
+++ src/app/_services/account.service.ts
@@ -0,0 +1,165 @@
+﻿import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
+import { BehaviorSubject, Observable } from 'rxjs';
+import { map, finalize } from 'rxjs/operators';
+
+import { environment } from '@environments/environment';
+import { Account, req } from '@app/_models';
+
+const baseUrl = `${environment.apiUrl}/accounts`;
+
+@Injectable({ providedIn: 'root' })
+export class AccountService {
+    private accountSubject: BehaviorSubject<Account | null>;
+    public account: Observable<Account | null>;
+
+    constructor(
+        private router: Router,
+        private http: HttpClient
+    ) {
+        this.accountSubject = new BehaviorSubject<Account | null>(null);
+        this.account = this.accountSubject.asObservable();
+    }
+
+    public get accountValue() {
+        return this.accountSubject.value;
+    }
+
+    login(email: string, password: string) {
+        return this.http.post<any>(`${baseUrl}/authenticate`, { email, password }, { withCredentials: true })
+            .pipe(map(account => {
+                this.accountSubject.next(account);
+                this.startRefreshTokenTimer();
+                return account;
+            }));
+    }
+
+    logout() {
+        this.http.post<any>(`${baseUrl}/revoke-token`, {}, { withCredentials: true }).subscribe();
+        this.stopRefreshTokenTimer();
+        this.accountSubject.next(null);
+        this.router.navigate(['/account/login']);
+    }
+
+    refreshToken() {
+        return this.http.post<any>(`${baseUrl}/refresh-token`, {}, { withCredentials: true })
+            .pipe(map((account) => {
+                this.accountSubject.next(account);
+                this.startRefreshTokenTimer();
+                return account;
+            }));
+    }
+
+    register(account: Account) {
+        return this.http.post(`${baseUrl}/register`, account);
+    }
+
+    verifyEmail(token: string) {
+        return this.http.post(`${baseUrl}/verify-email`, { token });
+    }
+
+    forgotPassword(email: string) {
+        return this.http.post(`${baseUrl}/forgot-password`, { email });
+    }
+
+    validateResetToken(token: string) {
+        return this.http.post(`${baseUrl}/validate-reset-token`, { token });
+    }
+
+    resetPassword(token: string, password: string, confirmPassword: string) {
+        return this.http.post(`${baseUrl}/reset-password`, { token, password, confirmPassword });
+    }
+
+    getAll() {
+        console.log("from account.ts")
+        console.log(baseUrl)
+        return this.http.get<Account[]>(baseUrl);
+    }
+// ****************** There is an error in calling the rout **************************************
+    getAllCareer() {
+        // console.log("getCareers from account.ts")
+        // console.log(`${baseUrl}/c`)
+        return this.http.get<Account>(`${baseUrl}/c`);
+    }
+// ****************** There is an error in calling the rout **************************************
+    getAllCareerN(career: string | any) {
+        console.log('from careerNRouts')
+        console.log(career)
+        return this.http.get<Account[]>(`${baseUrl}/N/${career}`);
+    }
+
+    getById(id: string |undefined) {
+        return this.http.get<Account[]>(`${baseUrl}/I/${id}`);
+    }
+
+    searchByName(name: string) {
+        // console.log("From account.ts :")
+        // console.log(name)
+        return this.http.get<Account>(`${baseUrl}/s/${name}`);
+    }
+
+    getByRole(role: string) {
+        return this.http.get<Account>(`${baseUrl}/${role}`);
+    }
+
+    sendReq(req: req) {
+        return this.http.post(`${baseUrl}/sendReq`, req);
+    }
+
+    sendReqAccept(boolean: string) {
+        return this.http.post(`${baseUrl}/sendReq/Accept`, boolean);
+    }
+
+    pendingReq(ProviderID: string | undefined) {
+        
+        return this.http.get<any[]>(`${baseUrl}/P_req/${ProviderID}`);
+    }
+
+
+
+    create(params: any) {
+        return this.http.post(baseUrl, params);
+    }
+
+    update(id: string, params: any) {
+        return this.http.put(`${baseUrl}/${id}`, params)
+            .pipe(map((account: any) => {
+                // update the current account if it was updated
+                if (account.id === this.accountValue?.id) {
+                    // publish updated account to subscribers
+                    account = { ...this.accountValue, ...account };
+                    this.accountSubject.next(account);
+                }
+                return account;
+            }));
+    }
+
+    delete(id: string) {
+        return this.http.delete(`${baseUrl}/${id}`)
+            .pipe(finalize(() => {
+                // auto logout if the logged in account was deleted
+                if (id === this.accountValue?.id)
+                    this.logout();
+            }));
+    }
+
+    // helper methods
+
+    private refreshTokenTimeout?: any;
+
+    private startRefreshTokenTimer() {
+        // parse json object from base64 encoded jwt token
+        const jwtBase64 = this.accountValue!.jwtToken!.split('.')[1];
+        const jwtToken = JSON.parse(atob(jwtBase64));
+
+        // set a timeout to refresh the token a minute before it expires
+        const expires = new Date(jwtToken.exp * 1000);
+        const timeout = expires.getTime() - Date.now() - (60 * 1000);
+        this.refreshTokenTimeout = setTimeout(() => this.refreshToken().subscribe(), timeout);
+    }
+
+    private stopRefreshTokenTimer() {
+        clearTimeout(this.refreshTokenTimeout);
+    }
+}
